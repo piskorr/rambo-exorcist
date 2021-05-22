085e1cadc2ec08bb73fba42ce1f8a15aa8454ceb
Jakub Piskorski <piskor.piskorski@gmail.com>
2021-05-22 17:30:55 +0200

    pick up message

--- rogue_like/Assets/Scripts/PickUpActionHandler.cs
+++ rogue_like/Assets/Scripts/PickUpActionHandler.cs
@@ -1,29 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpActionHandler : MonoBehaviour
 {
     public GameObject weaponPrefab;
 
+    [SerializeField]
+    private Text pickUpText;
     private WeaponHolderController weaponHolderController;
-
+    private bool pickUpAllowed;
 
 
     void Start()
     {
         weaponHolderController = GameObject.FindGameObjectWithTag("Player").GetComponent<WeaponHolderController>();
+        pickUpText.gameObject.SetActive(false);
     }
 
 
-    void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collider.gameObject.CompareTag("Player"))
+        if (collision.gameObject.name.Equals("Player"))
         {
-            if(weaponHolderController.AddWeapon(weaponPrefab))
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+
+            if (weaponHolderController.AddWeapon(weaponPrefab))
             {
                 gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
         }
     }
 }
